d44b7739202071ea1c3060992dd3d73378ce7575
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-12-23 17:04:59 +0200

    add user table

--- src/components/user.js
+++ src/components/user.js
@@ -1,10 +1,22 @@
 import { getData } from "../api";
+import { createUserTable } from "../markup";
+import { updateList } from "../utils";
 
 const userId = location.search.split("=")[1] || 1;
 
-console.log(userId);
-getData('users/' + userId)
-  .then((resp) => {
-    console.log(resp);
-  })
-  .catch((error) => console.log(error));
+const userTableRef = document.querySelector(".js-user-table");
+
+async function getDataUser() {
+  try {
+    const response = await getData("users/" + userId);
+
+    const markup = createUserTable(response);
+    updateList(markup, userTableRef);
+
+    console.log(markup);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getDataUser();
--- src/markup/index.js
+++ src/markup/index.js
@@ -1,9 +1,31 @@
-export function createUsersTable ({website, email, id, name, username}) {
+export function createUsersTable({ website, email, id, name, username }) {
   return ` <tr class="tr-row" data-user=${id}>
             <th scope="col">${id}</th>
             <th scope="col">${name}</th>
             <th scope="col">${username}</th>
             <th scope="col">${email}</th>
             <th scope="col">${website}</th>
-          </tr>`
+          </tr>`;
+}
+
+export function createUserTable({
+  website,
+  email,
+  id,
+  name,
+  username,
+  company,
+  address,
+  phone,
+}) {
+  return `<tr data-userid='${id}'>
+  <th scope="row">1</th>
+  <td>${name}</td>
+  <td>${username}</td>
+  <td>${email}</td>
+  <td>${address.city}</td>
+  <td>${phone}</td>
+  <td>${website} </td>
+  <td>${company.name}</td>
+</tr>`;
 }
